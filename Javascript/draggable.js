8b554048be8af0049848056ae3dc3d71849789fd
jeff <jkwok97@gmail.com>
2018-11-01 10:10:17 -0600

    2018_11_01 morning

---
+++ Javascript/draggable.js
@@ -0,0 +1,41 @@
+
+function makeDraggable(evt) {
+    var svg = evt.target;
+    svg.addEventListener('mousedown', startDrag);
+    svg.addEventListener('mousemove', drag);
+    svg.addEventListener('mouseup', endDrag);
+    svg.addEventListener('mouseleave', endDrag);
+
+    function getMousePosition(evt) {
+      var CTM = svg.getScreenCTM();
+      return {
+        x: (evt.clientX - CTM.e) / CTM.a,
+        y: (evt.clientY - CTM.f) / CTM.d
+      };
+    }
+
+    var selectedElement, offset;
+
+    function startDrag(evt) {
+      if (evt.target.classList.contains('draggable')) {
+        selectedElement = evt.target;
+        offset = getMousePosition(evt);
+        offset.x -= parseFloat(selectedElement.getAttributeNS(null, "x"));
+        offset.y -= parseFloat(selectedElement.getAttributeNS(null, "y"));
+      }
+    }
+
+    function drag(evt) {
+      if (selectedElement) {
+        evt.preventDefault();
+        var coord = getMousePosition(evt);
+        selectedElement.setAttributeNS(null, "x", coord.x - offset.x);
+        selectedElement.setAttributeNS(null, "y", coord.y - offset.y);
+      }
+    }
+
+    function endDrag(evt) {
+      selectedElement = null;
+    }
+  }
+ 
--- Udemy/Flask_Tutorial/Adoption_site/adoption_site.py
+++ Udemy/Flask_Tutorial/Adoption_site/adoption_site.py
@@ -2,20 +2,22 @@
 #ADOPTION_SITE.PY
 
 import os
-from forms import AddForm, DelForm
+from adoption_forms import AddForm, DelForm
 from flask import Flask, render_template, url_for,redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = "mysecretkey"
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config['SECRET_KEY'] = "mykey"
 
 ######################################
 #####  SQL DATABASE SECTION   ########
 ######################################
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
