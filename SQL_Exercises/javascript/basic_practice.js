d9a881fff3113e5e82ac892253875547f653cee7
jeff <jkwok97@gmail.com>
2018-11-05 12:19:21 -0700

    2018_11_05

---
+++ SQL_Exercises/javascript/basic_practice.js
@@ -0,0 +1,70 @@
+
+
+var myModule = (function() {
+    var myString = 'My Value';
+    console.log("Hello World!")
+    function myFunc() {
+        console.log('I am in myFunc');
+    }
+
+    return {
+        stuff : 'some stuff',
+        myNum : 42,
+        yourString : myString,
+        yourFunc : myFunc
+    };
+
+})();
+
+console.log(myModule);
+myModule.yourFunc();
+console.log(myModule.yourString);
+
+function moveText() {
+    var x = document.getElementById("text-area");
+    document.getElementById("moved-text").innerHTML = x.value;
+    x.value = '';
+}
+
+
+var playModule = function() {
+    document.getElementById("json").onclick = screenPrint;
+    document.getElementById("array").onclick = myArray;
+    document.getElementById("object").onclick = myObject;
+    
+    var myArray = [1,2,3,4,5,6];
+    var myObject = {
+        firstName: 'John',
+        lastName: 'Smith',
+        birthYear: 1990,
+        family: ['Jane', 'Mark', 'Bob', 'Emily'],
+        job: 'teacher',
+        isMarried: false
+    }
+
+    function myArray() {
+        for (var i = 0; i < myArray.length; i++)
+        console.log(myArray[i]);
+    }
+    
+    function myObject() {
+        Object.keys(myObject).forEach(function(key) {
+        console.log(key, myObject[key]);
+            });
+    }
+
+    // ALTERNATE LOOP FOR MYOBJECT
+    // function myObject() {
+    //     for (i in myObject) {
+    //         console.log(i, myObject[i]);
+    //     }
+    // }
+
+    function screenPrint() {
+        console.log("this is Json");
+    }
+    return {
+        
+    }
+}();
+
---
+++ Udemy/Javascript_Tutorial/Section_6/app_section6.js
@@ -0,0 +1,14 @@
+// BUDGET CONTROLLERS
+
+var budgetController = (function () {
+    var x = 23;
+
+    var add = function(a) {
+        return x + a;
+    }
+    return {
+        publicTest: function(b) {
+            console.log(add(b));
+        } 
+    }
+})();
