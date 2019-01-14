297c4d8e407a27a04b600d899a46efc5c3c48d4a
Jeff Kwok <jkwok97@gmail.com>
2019-01-14 08:46:58 -0700

    2019_01_14

---
+++ Udemy/Web_Developer2/next-ssr/components/Image.js
@@ -0,0 +1,5 @@
+const Image = () => (
+    <img src='https://images.unsplash.com/photo-1547241508-f95f5c4c1305?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80'></img>
+);
+
+export default Image
