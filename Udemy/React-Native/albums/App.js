c3325141d8ce642d1de35e3002c384a51785cc85
Jeff Kwok <jkwok97@gmail.com>
2019-01-27 21:52:18 -0700

    1st react-native app complete 2019_01_27

--- Udemy/React-Native/albums/App.js
+++ Udemy/React-Native/albums/App.js
@@ -1,32 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.textInput}>Hello World!</Text>
-        <Text style={styles.textInput}>Hello Lemi!!</Text>
-        <Text style={styles.textInput}>Hello Jeff!</Text>
-        <Text style={styles.textInput}>Hello Ana!</Text>
-        <Text style={styles.textInput}>Hello Larry!</Text>
-        <Text style={styles.textInput}>Hello Ray!</Text>
+      <View style= {{flex: 1}}>
+        <Header headerText={'Albums'}/>
+        <AlbumList />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "black",
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: "white"
-  },
-  textInput: {
-    color: "white",
-  }
-});
+
 
 
---
+++ Udemy/React-Native/albums/src/components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header = (props) => {
+    const { textStyle, viewStyle } = styles;
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>{props.headerText}</Text>
+        </View>
+    )};
+
+const styles = {
+    viewStyle: {
+        backgroundColor: "#F8F8F8",
+        justifyContent: "center",
+        alignItems: "center",
+        height: 100,
+        paddingTop: 40,
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 5 },
+        shadowOpacity: 0.5,
+        elevation: 2,
+        position: "relative"
+    },
+    textStyle: {
+        fontSize: 20,
+        // fontFamily: ""
+    }
+  };
+
+export default Header;
