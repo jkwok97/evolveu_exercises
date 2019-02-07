9ef8360f3ff73fc58d5223fa4f16774dcada9234
Jeff Kwok <jkwok97@gmail.com>
2019-02-06 22:07:43 -0700

    started animation for react-native

---
+++ Udemy/React-Native/Advanced/animate/src/Deck.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { View, Animated, PanResponder } from 'react-native';
+
+class Deck extends Component {
+    constructor(props) {
+        super(props);
+        const position = new Animated.ValueXY();
+        const panResponder = PanResponder.create({
+            onStartShouldSetPanResponder: () => true,
+            onPanResponderMove: (event, gesture) => {
+                position.setValue({ x: gesture.dx, y: gesture.dy })
+            },
+            onPanResponderRelease: () => {}
+        })
+        this.panResponder = panResponder;
+        this.position = position;
+    }
+
+    getCardStyle() {
+        return {
+            ...this.position.getLayout(),
+            transform: [{ rotate: '45deg' }]
+        };
+    }
+
+    renderCards() {
+        return this.props.data.map((item, index) => {
+            if (index === 0) {
+                return (
+                    <Animated.View 
+                        key={item.id}
+                        style={this.getCardStyle()}
+                        {...this.panResponder.panHandlers}
+                    >
+                        {this.props.renderCard(item)}
+                    </Animated.View>
+                )
+            }
+            return this.props.renderCard(item);
+        })
+    }
+    render() {
+        return (
+            <View>
+                {this.renderCards()}
+            </View>
+        );
+    }
+}
+
+export default Deck;
