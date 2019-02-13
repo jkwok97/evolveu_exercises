2b5e3e1d474a5e9e0a1cc2d6fc0bb866a12f8b15
Jeff Kwok <jkwok97@gmail.com>
2019-02-12 22:12:35 -0700

    completed created user in firebase for one-time-password app

---
+++ Udemy/React-Native/Advanced/one-time-password/functions/create_user.js
@@ -0,0 +1,20 @@
+const admin = require('firebase-admin');
+
+module.exports = (req, res) => {
+    // verify the user provided a phone
+    if (!req.body.phone) {
+        return res.status(422).send({ error: 'Bad Input' });
+    } 
+
+    // format the phone number to remove dashes and parens
+    const phone = String(req.body.phone).replace(/[^\d]/g, '');
+
+    // create a new user account
+    admin.auth().createUser({ uid: phone })
+    .then(user => res.send(user))
+    .catch(err => res.status(422).send({ error: err }));
+
+    // respond to user request, saying the account was made
+
+}
+
