8f10ed7cd561bdc091d68e4ef6efb0cece47af89
Jeff Kwok <jkwok97@gmail.com>
2019-02-14 17:28:01 -0700

    finished one-time-password app with react-native

---
+++ Udemy/React-Native/Advanced/firebase-auth/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import firebase from 'firebase';
+import SignUpForm from './components/SignUpForm';
+import SignInForm from './components/SignInForm';
+
+const { config } = require('./setup');
+
+export default class App extends React.Component {
+  componentDidMount() {
+    firebase.initializeApp(config);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <SignUpForm />
+        <SignInForm />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+});
